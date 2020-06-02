45e7d9cc15aaf750926b4ed526e68f1363a3646f
Mladen Mikic <mb87.apple@gmail.com>
2020-06-02 11:01:24 +0200

    Bug fix - fixed macOS 10.14. compatibility bug. SwiftUI can’t be loaded (without hacks) under mojave

--- Application/AppDelegate.swift
+++ Application/AppDelegate.swift
@@ -31,20 +31,6 @@ import Cocoa
 		{
 			self.installScript(self)
 		}
-//		// Create the SwiftUI view that provides the window contents
-//
-//		let contentView = ContentView()
-//
-//		// Create the window and set the content view
-//
-//		window = NSWindow(
-//		    contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-//		    styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-//		    backing: .buffered, defer: false)
-//		window.center()
-//		window.setFrameAutosaveName("Main Window")
-//		window.contentView = NSHostingView(rootView: contentView)
-//		window.makeKeyAndOrderFront(nil)
 	}
 
 
